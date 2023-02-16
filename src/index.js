5774679092591f95dcce7308b0be0af7db752a9a
global-p0lluxstar <global-web-registra@yandex.ru>
2023-02-16 12:32:59 +0300

    160223-1232

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
+/* module.exports = function check(str, bracketsConfig) {
   // your solution
 }
+ */
+
+function check(str, bracketsConfig) {
+ let count = 0;
+ let arr = [];
+ for(let i = 0; i < bracketsConfig.length; i++){
+  arr = arr.concat(bracketsConfig[i])
+ }
+
+arr.forEach(el => {
+  for(let i = 0; i < str.length; i++){
+    if(el === str[i]){
+      count++
+    }
+  }
+})
+ console.log(arr)
+ console.log(count)
+}
+
+check('[]()(', [['(', ')'], ['[', ']']]);
