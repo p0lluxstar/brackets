d2a10fac27e723daadcdeb3d5f087e3cea271381
global-p0lluxstar <global-web-registra@yandex.ru>
2023-02-18 00:18:47 +0300

    180223-00:18

--- src/index.js
+++ src/index.js
@@ -1,24 +1,67 @@
-/* module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
- */
+module.exports = function check(str, bracketsConfig) {
 
-function check(str, bracketsConfig) {
- let count = 0;
- let arr = [];
- for(let i = 0; i < bracketsConfig.length; i++){
-  arr = arr.concat(bracketsConfig[i])
- }
-
-arr.forEach(el => {
-  for(let i = 0; i < str.length; i++){
-    if(el === str[i]){
-      count++
+  let newStr = str;
+  for(let i = 0; i<str.length; i++){
+    if(i % 2 === 0){
+      newStr = newStr.replace('|', '{')
+    }else {
+      newStr = newStr.replace('|', '}')
     }
   }
-})
- console.log(arr)
- console.log(count)
-}
 
-check('[]()(', [['(', ')'], ['[', ']']]);
+  let newStr2 = newStr;
+  for(let i = 0; i<str.length; i++){
+    if(i % 2 === 0){
+      newStr2 = newStr2.replace('7', '(')
+    }else {
+      newStr2 = newStr2.replace('7', ')')
+    }
+  }
+
+  let newStr3 = newStr2;
+  for(let i = 0; i<str.length; i++){
+    if(i % 2 === 0){
+      newStr3 = newStr3.replace('8', '{')
+    }else {
+      newStr3 = newStr3.replace('8', '}')
+    }
+  }
+
+  console.log(newStr3)
+    
+  const open = ['(', '[', '{', '1', '3', '5']
+  let stack = [];
+
+  let obj ={
+    [')'] : '(',
+    [']'] : '[',
+    ['}'] : '{',
+    '2' : '1',
+    '4' : '3',
+    '6' : '5'
+  }
+
+  for(let i = 0; i < newStr3.length; i++){
+    let symbol = newStr3[i];
+
+    if (open.includes(symbol)){
+      stack.push(symbol)
+    } else {
+      if(stack.length === 0){
+        return false;
+      }
+    }
+
+    let topEl = stack[stack.length - 1]
+
+    if( obj[symbol] === topEl){
+      stack.pop();
+    }    
+  }
+
+  if(stack.length === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
